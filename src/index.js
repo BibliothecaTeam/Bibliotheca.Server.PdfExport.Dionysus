42e9f77b336152e8d851f8036533b46ffe362a0f
Marcin Czachurski <mczachurski@icloud.com>
2017-05-06 18:08:21 +0200

    Add api for transformating markdown to pdf.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
-var http = require('http');
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var markdownpdf = require("markdown-pdf");
 
-var server = http.createServer(function(request, response) {
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!");
+// set our port
+var port = process.env.PORT || 8080;
 
+// get an instance of the express Router
+var router = express.Router();
+
+// health route
+router.get('/health', function(req, res) {
+    res.send('[Dionysos: 1.0.0]  I\'m alive and reachable');   
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+// generator route
+router.post('/transform', function(req, res) {
 
-console.log("Server running at http://localhost:%d", port);
+    markdownpdf().from.string(req.body).to.string(function (error, pdfString) {
+        if(error) {
+            res.statusCode = 400;
+            res.send(error);
+        }
+        else {
+            console.log("PDF created successfully");
+            res.statusCode = 200;
+            res.contentType = "application/pdf";
+            res.send(pdfString);
+        }
+    });
+});
+
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// start server
+app.listen(port);
+console.log('Magic happens on port ' + port);
